a15564e1c6a1e71a661e91f6f1512df32af17490
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-06 17:48:25 +0530

    updated

--- DataTypeExamples/PrimitiveDataTypes.java
+++ DataTypeExamples/PrimitiveDataTypes.java
@@ -1,5 +1,29 @@
 package DataTypeExamples;
 
 public class PrimitiveDataTypes {
-    
+    public static void main(String[] args) {
+
+    // Integer types
+    byte bytenub = 80;
+    short shortnub =8080;
+    int intnub =8888;
+    long longnub =8888888888L; //'L' for long
+
+    //Float types
+    float floatnub = 8.88f; // 'f' for float
+    double doublenub = 88.8d; // 'd' for double
+
+    // Character and boolean
+    char charletter = 'G';
+    boolean isboolean =true;
+
+    System.out.println("Byte " + bytenub);
+    System.out.println("Short " + shortnub);
+    System.out.println("Int " + intnub);
+    System.out.println("Long " + longnub);
+    System.out.println("Float " + floatnub);
+    System.out.println("Double " + doublenub);
+    System.out.println("Char " + charletter);
+    System.out.println("Boolean " + isboolean);
+}
 }
