e4eb0adcaff6b959a84fd49593395c39f2652797
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-07 18:25:14 +0530

    updated

---
+++ InputOutput3/OutputExample.java
@@ -0,0 +1,10 @@
+package InputOutput3;
+
+public class OutputExample {
+    public static void main(String[] args){
+        System.out.print("Prints Text without adding new line");
+        System.out.println(",  prints text and moves to next line");
+        System.out.print("prints this text below the println text");
+    }
+    
+}
