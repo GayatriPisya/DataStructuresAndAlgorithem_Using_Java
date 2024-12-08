efd39fb9d13f4978782fd891a4efbea75c49b84f
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-08 21:31:59 +0530

    Example for switch case

---
+++ ConditionalStatements4/SwitchCaseExample.java
@@ -0,0 +1,31 @@
+//Question: Check the day of the week
+package ConditionalStatements4;
+
+public class SwitchCaseExample {
+    public static void main(String[] args){
+        int day = 3;
+
+        switch(day){
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("Tuesday");
+            break;
+            case 3:
+            System.out.println("Wednesday");
+            break;
+            case 4:
+            System.out.println("Thursday");
+            break;
+            case 5:
+            System.out.println("Friday");
+            break;
+            case 6:
+            System.out.println("Saturday");
+            break;
+            default:
+            System.out.println("Sunday");
+        }
+    }
+}
