cb313d68a3ac473780ebbe3a0c1dfb60299b471a
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-08 21:43:09 +0530

    Fall through example

---
+++ ConditionalStatements4/FallThroughExample.java
@@ -0,0 +1,18 @@
+//If you do not include a break statement, the program will continue executing all cases after the matched case.
+package ConditionalStatements4;
+public class FallThroughExample {
+    public static void main(String[] args) {
+        int day = 2;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+            case 2:
+                System.out.println("Tuesday");
+            case 3:
+                System.out.println("Wednesday");
+            default:
+                System.out.println("Weekend");
+        }
+    }
+}
