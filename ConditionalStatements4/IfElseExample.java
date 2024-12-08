fec5d7e7d0ded8a936d6d0abb7dbcba26b9ac27e
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-08 17:50:59 +0530

    if else example

---
+++ ConditionalStatements4/IfElseExample.java
@@ -0,0 +1,20 @@
+package ConditionalStatements4;
+
+public class IfElseExample {
+    public static void main(String[] args){
+        String Name = "Gayatri";
+
+        if(Name == "Gayatri"){
+            System.out.println(" If condition is true");
+        } else{
+            System.out.println("If condition is not true");
+        }
+
+        if(Name == "Geetha"){
+            System.out.println(" If condition is true");
+        }else{
+            System.out.println("If condition is not true");
+        }
+    }
+    
+}
