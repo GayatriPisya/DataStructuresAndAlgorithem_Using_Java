5ab55a7d95e5325bdaafb3082d20ed24269d5c2c
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-08 17:34:16 +0530

    If condition true code

---
+++ ConditionalStatements4/IfConditionTrue.java
@@ -0,0 +1,13 @@
+package ConditionalStatements4;
+
+public class IfConditionTrue {
+    public static void main(String[] args){
+        int g = 8 ;
+
+        if(g > 5){
+            System.out.println("This is is a true if condition");
+        }
+        System.out.println("End of the code");
+    }
+    
+}
