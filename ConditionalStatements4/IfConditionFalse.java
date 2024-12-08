e1f19fd329dd8ec950779a56ac047f2df989b2ff
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-08 17:37:52 +0530

    updated the if condition false statement

---
+++ ConditionalStatements4/IfConditionFalse.java
@@ -0,0 +1,13 @@
+package ConditionalStatements4;
+
+public class IfConditionFalse {
+    public static void main(String[] args){
+        int g = 88;
+
+        if(g < 80){
+            System.out.println("This is is a true if condition, if this line not printed the this is a false condition");
+        }
+        System.out.println("End of the code");
+    }
+    
+}
