8ee1d391f3a4fe4a3665d9debb559f1dc13077ad
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-09 22:04:10 +0530

    for loop example

---
+++ LoopsExamples5/ForLoopExample.java
@@ -0,0 +1,9 @@
+package LoopsExamples5;
+
+public class ForLoopExample {
+    public static void main(String[] args){
+        for(int i = 5; i <= 8; i++){
+        System.out.println("Number is: "+ i);
+        }
+    }
+}
