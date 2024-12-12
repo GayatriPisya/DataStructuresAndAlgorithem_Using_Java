d4eea151280a1ce65c9585991d7045e875099262
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-12 11:10:41 +0530

    example for no return type function

---
+++ FunctionsExamples6/FunctionExample.java
@@ -0,0 +1,12 @@
+package FunctionsExamples6;
+
+public class FunctionExample {
+    public static void greet(){
+        System.out.println("Hello!");
+    }
+    public static void main(String[] args){
+        greet();
+
+    }
+    
+}
