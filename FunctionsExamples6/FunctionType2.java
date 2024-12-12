97f142e946207349579cdf3259b4732e6abc03ec
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-12 11:28:22 +0530

    example for function with Accepts input but does not return a value.

--- FunctionsExamples6/FunctionType2.java
+++ FunctionsExamples6/FunctionType2.java
@@ -1,5 +1,11 @@
 package FunctionsExamples6;
 
 public class FunctionType2 {
+    public static void main(String[] args){
+        displaysum(5 , 8);
+    }
+    public static void displaysum(int a, int b){
+        System.out.println("The sum is " + (a + b));
+    }
     
 }
