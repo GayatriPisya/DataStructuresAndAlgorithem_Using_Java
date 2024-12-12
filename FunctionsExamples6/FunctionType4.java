10457e21d69bb075296593f607842b307ebefedd
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-12 12:14:33 +0530

    example for function Accepts input and returns a value.

---
+++ FunctionsExamples6/FunctionType4.java
@@ -0,0 +1,11 @@
+package FunctionsExamples6;
+
+public class FunctionType4 {
+    public static void main(String[] args){
+        int result = displaysum(6 , 8);
+        System.out.println("Sum of a + b is " + result);
+    }
+    public static int displaysum(int a ,int b){
+        return a + b;
+        }
+}
