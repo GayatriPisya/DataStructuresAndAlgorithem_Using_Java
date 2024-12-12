a56e36eab3d703fe54e979f1cc40e40e368be191
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-12 11:45:22 +0530

    example for function with Does not accept input but returns a value.

---
+++ FunctionsExamples6/FunctionType3.java
@@ -0,0 +1,12 @@
+package FunctionsExamples6;
+
+public class FunctionType3 {
+    public static int Number(){
+        return 88;
+    }
+    public static void main(String[] args){
+        int result = Number();
+        System.out.println("The Number is " + result);
+    }
+   
+}
