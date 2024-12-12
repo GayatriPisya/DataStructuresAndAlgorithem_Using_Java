c1961fa7c6ab15691ec172b3d323366868c4d645
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-12 11:14:19 +0530

    updated

--- FunctionsExamples6/FunctionExample.java
+++ FunctionsExamples6/FunctionExample.java
@@ -1,5 +1,6 @@
 package FunctionsExamples6;
 
+// These functions perform an action but do not accept input or return any value
 public class FunctionExample {
     public static void greet(){
         System.out.println("Hello!");
