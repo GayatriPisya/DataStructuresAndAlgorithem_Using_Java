cf54b37efcbd065537aac3a8c4ed7dbf200d7b23
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-15 12:39:02 +0530

    example for While loop travers

---
+++ ArrayExamples9/TraversalsinArrays/WhileLoopTraversal.java
@@ -0,0 +1,14 @@
+package ArrayExamples9.TraversalsinArrays;
+
+public class WhileLoopTraversal {
+    public static void main(String[] args){
+        int[] nubs = {1,2,3,4,5,6,7,8,9,10};
+        int i = 0;
+        System.out.println("While loop travers");
+        while(i < nubs.length){
+            System.out.println(nubs[i]);
+            i++;
+        }
+    }
+    
+}
