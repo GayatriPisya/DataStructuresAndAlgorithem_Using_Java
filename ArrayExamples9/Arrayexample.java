29084279f6442b06afc079b1e82642aefd9119e8
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-15 12:20:24 +0530

    example for array

---
+++ ArrayExamples9/Arrayexample.java
@@ -0,0 +1,13 @@
+package ArrayExamples9;
+
+public class Arrayexample {
+    public static void main(String[] args){
+        int[] nubs = {2,4,6,8};
+        System.out.println("Number is " + nubs[0]);
+        System.out.println("Number is " + nubs[1]);
+
+        nubs[2] = 5; // Changing the third Number
+        System.out.println("Number is " + nubs[2]);
+    }
+    
+}
