3140b1640a4ff0e86a71f2bd49ef1b05510f5887
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-15 13:04:14 +0530

    updated

---
+++ ArrayExamples9/ArraySizeExample.java
@@ -0,0 +1,13 @@
+package ArrayExamples9;
+
+public class ArraySizeExample {
+    public static void main(String[] args){
+        int[] arr = new int[5];
+        //  arr[0] = 2;
+        //  arr[1] = 4;
+        //  arr[2] = 6;
+        //  arr[3] = 8;
+        //  arr[4] = 10;
+         System.out.println("Length of the array is: " + arr.length);
+    }
+}
