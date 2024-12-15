b2401bf08e3b5bfd7cac072e223ecfca6a1e183f
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-15 12:33:52 +0530

    example for ForEach Traversal

---
+++ ArrayExamples9/TraversalsinArrays/ForEachTraversal.java
@@ -0,0 +1,12 @@
+package ArrayExamples9.TraversalsinArrays;
+
+public class ForEachTraversal {
+    public static void main(String[] args){
+        String[] names = {"Sireesha", "Kishore", "Gayatri"};
+
+        for( String name : names){
+            System.out.println(" Name is " +name);
+        }
+    }
+    
+}
