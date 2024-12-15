86532d9e6dbc4c4db4042bf6eea880179bf3014e
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-15 12:28:07 +0530

    example for For loop iterations

---
+++ ArrayExamples9/TraversalsinArrays/ForLoopTraversal.java
@@ -0,0 +1,13 @@
+package ArrayExamples9.TraversalsinArrays;
+
+public class ForLoopTraversal {
+    public static void main(String[] args){
+        int[] arr = {8,14,15};
+        System.out.println("For loop iterations : ");
+
+        for(int i = 0 ; i < arr.length; i++){
+            System.out.println("Number of index " + i + " is " + arr[i]);
+        }
+    }
+    
+}
