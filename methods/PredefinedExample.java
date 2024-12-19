8822a632e2d8857e62bf535b36f2a2eef0582d8c
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-19 17:23:33 +0530

    Predefined example

--- methods/PredefinedExample.java
+++ methods/PredefinedExample.java
@@ -1,5 +1,10 @@
 package methods;
 
 public class PredefinedExample {
+    public static void main(String[] args){
+        String name = "Gayatri";
+        System.out.println("Length of the string is : " + name.length());
+        System.out.println("Lower to upper case : " + name.toUpperCase());
+    }
     
 }
