9dda3eb4b3af446a913a81a55a5db025eaf16e01
Gayatri Pisya <gayatripisya@gmail.com>
2024-12-25 17:51:53 +0530

    .

--- Searching10/BinarySearchStringExample.java
+++ Searching10/BinarySearchStringExample.java
@@ -2,28 +2,29 @@ package Searching10;
 
 public class BinarySearchStringExample {
     public static void main(String[] args){
-        int[] arr = {1,2,3,4,5,6,7,8,9};
-        int target = 8;
-        int index = binarysearch(arr, target);
+        String[] arr = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        String target = "g";
+        int index = Binarysearch(arr, target);
         if(index != -1){
             System.out.println("Element found at index " + index);
-        } else {
+        }else {
             System.out.println("Element not found");
         }
     }
-    public static int binarysearch(int[] arr, int target){
-        int low =0;
-        int high = arr.length-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(arr[mid]==target){
+    public static int Binarysearch(String[] arr, String target){
+        int low = 0;
+        int high = arr.length - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(arr[mid].equals(target)){
                 return mid;
-            }else if(arr[mid] < target){
-                low = mid +1;            
-            }else if(arr[mid] > target){
-                low = mid -1;
+            }else if(arr[mid].compareTo(target) < 0){
+                low = mid + 1;
+            }else if(arr[mid].compareTo(target) > 0){
+                high = mid - 1;
             }
         }
         return -1;
     }
+    
 }
