f1d4afd7eee8ec7de4ce97994103ebd6f0f7c886
Gayatri Pisya <gayatripisya@gmail.com>
2025-07-16 22:27:24 +0530

    time complexity

---
+++ complexity/time.java
@@ -0,0 +1,8 @@
+public class time{
+    public static void main(String args[]){
+        int x=2, y=4;
+        int sum = x+y;
+        System.out.println("The sum is "+ sum+" .");
+    }
+}
+// For this problem the time complexity is O(1) because the operations performed (addition and printing) do not depend on the size of the input. The space complexity is also O(1) since we are using a fixed amount of space for variables regardless of input size.
